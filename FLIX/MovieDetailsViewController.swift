73e42c1a8fe560c28e04513e9bcaa41da48feec4
dustinburda <dustin99wii@gmail.com>
2022-03-07 06:51:12 -0600

    Finished

--- FLIX/MovieDetailsViewController.swift
+++ FLIX/MovieDetailsViewController.swift
@@ -6,13 +6,42 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleView: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    var movie: [String:Any]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let title = movie["title"] as? String
+        titleView.text = title
+        titleView.sizeToFit()
 
-        // Do any additional setup after loading the view.
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        
+        let posterURL = URL(string: baseURL + posterPath)
+          
+        posterView.af_setImage(withURL: posterURL!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        
+        let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropURL!)
+
+        // Do any additional setup after  loading the view.
     }
     
 
--- FLIX/MovieViewController.swift
+++ FLIX/MovieViewController.swift
@@ -50,6 +50,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
         let task = session.dataTask(with: request) { (data, response, error) in
              // This will run when the network request returns
              if let error = error {
@@ -67,7 +68,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
              }
         }
         task.resume()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
         
+        //Find movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
